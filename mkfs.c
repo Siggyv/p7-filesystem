d674ea7deec7eb1b7daf1b223b3396ac06e65dea
Siggy <svanvalkenbu@wisc.edu>
2024-04-18 02:30:27 -0500

    made mkfs, arg parsing should be good, havent tested initialization

---
+++ mkfs.c
@@ -0,0 +1,115 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "wfs.h"
+
+// PRESUMING: You may presume the block size is always 512 bytes (according to instructions)
+
+int main(int argc, char ** argv) {
+    int num_blocks = -1;
+    int num_inodes = -1;
+    char * DISK_IMG_PATH = NULL;
+
+    // argument parsing
+    for(int i = 1; i < argc; i++) {
+        if(strcmp(argv[i], "-d") == 0) {
+            DISK_IMG_PATH = (char *)malloc(strlen(argv[i+1]) + strlen("./") + 1);
+            if(DISK_IMG_PATH == NULL) {
+                perror("ERROR: could not get memory for the disk image path.\n");
+                exit(1);
+            }
+            strcpy(DISK_IMG_PATH, "./");
+            strcat(DISK_IMG_PATH, argv[i+1]);
+            // replace all _ with a . go backwards, and only do once.
+            for(int j = strlen(DISK_IMG_PATH); j > 0; j--) 
+            {
+                if(DISK_IMG_PATH[j] == '_')
+                {
+                    DISK_IMG_PATH[j] = '.';
+                }
+            }
+            i++; // skip the next arg
+        }
+        else if(strcmp(argv[i], "-i") == 0) {
+            num_inodes = atoi(argv[i+1]);
+            i++;
+        }
+        else if(strcmp(argv[i], "-b") == 0) {
+            num_blocks = atoi(argv[i+1]);
+            i++;
+        }
+    }
+    if(num_blocks == -1 || num_inodes == -1 || DISK_IMG_PATH == NULL) {
+        printf("USAGE: -d {fs img path} -i {number i nodes} -b {number data blocks}\n");
+        if(DISK_IMG_PATH != NULL) {
+            free(DISK_IMG_PATH);
+        }
+        exit(1);
+    }
+
+    // round up num blocks to nearest higher multiple of 32
+    if(num_blocks % 32 != 0)
+        num_blocks = (num_blocks + 31) & ~31;
+
+    // check that num of blocks is possible with disk img file size
+    struct stat statbuf;
+    if(stat(DISK_IMG_PATH, &statbuf) != 0) {
+        perror("ERROR: cannot get disk image size.\n");
+        free(DISK_IMG_PATH);
+        exit(1);
+    }
+
+    if(num_blocks * BLOCK_SIZE > statbuf.st_size) {
+        perror("ERROR: block size is too large to fit in disk image.\n");
+        free(DISK_IMG_PATH);
+        exit(1);
+    }
+
+    // initialize super block
+    // open disk img
+    int fd = open(DISK_IMG_PATH, O_RDWR);
+    if(fd < 0) {
+        perror("ERROR: failed to open disk image for initialization.\n");
+        exit(1);
+    }
+
+    // define super block
+    struct wfs_sb * superblock = (struct wfs_sb *)malloc(sizeof(struct wfs_sb));
+    off_t i_map_ptr = sizeof(struct wfs_sb);
+    off_t d_map_ptr = i_map_ptr + num_inodes;
+    off_t i_block_ptr = d_map_ptr + num_blocks;
+    off_t d_block_ptr = i_block_ptr + (sizeof(struct wfs_inode) * num_inodes);
+    superblock->num_data_blocks = num_blocks;
+    superblock->num_inodes = num_inodes;
+    superblock->i_bitmap_ptr = i_map_ptr;
+    superblock->d_bitmap_ptr = d_map_ptr;
+    superblock->i_blocks_ptr = i_block_ptr;
+    superblock->d_blocks_ptr = d_block_ptr;
+
+    // write super block to beginning of disk img
+    lseek(fd, 0, SEEK_SET);
+    write(fd, (void *)superblock, sizeof(superblock));
+
+    // initialize root inode; CONSIDER: may need to zero the bitmaps, dont think so so I wont do it.
+    struct wfs_inode * inode = (struct wfs_inode *) malloc(sizeof(struct wfs_inode));
+    inode->num = 0; // NOTE ROOT DIRECTORY IS FIRST INODE
+    inode->mode = 0; // change online says S_IFDIR | 0755, asked piazza
+    inode->uid = 0;
+    inode->gid = 0;
+    inode->size = 2 * sizeof(struct wfs_dentry); // for . and .. dirs
+    inode->nlinks = 2;
+    inode->atim = inode->mtim = inode->ctim = time(NULL);
+    for(int i = 0; i < N_BLOCKS; i++) {
+        inode->blocks[i] = 0;
+    }
+
+    // write at correct spot
+    lseek(fd, superblock->i_blocks_ptr, SEEK_SET);
+    write(fd, inode, sizeof(struct wfs_inode));
+
+    free(DISK_IMG_PATH);
+    return 0;
+}
