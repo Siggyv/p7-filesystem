78944b994b2a1f6b4be255840e699c7c2dc90e37
Adam Rosler <rosler@royal-19.cs.wisc.edu>
2024-04-26 23:02:13 -0500

    started to add a inode exists checker

--- wfs.c
+++ wfs.c
@@ -15,12 +15,53 @@
 
 
 
-struct wfs_sb *file_system; //define a file system we can use
+char* file_system; //define a file system we can use
+struct wfs_sb* super_block;
 
+//return 0 on success, -1 on failure
+//fills up a given inode, given a path of a inode
+int get_inode(const char *path, struct wfs_inode *inode){
+    //fetch the root inode
+    struct wfs_inode *curr_inode = (struct wfs_inode *) (file_system + super_block->i_blocks_ptr);
+    //loop through the path
+    char* token;
+    char* copy_path = strdup(path);
+    struct wfs_dentry* datablock;
+    int i;
+    int found; //ensures the path has been found
+    while((token = strsep(&copy_path, "/")) != NULL){
+        //nothing found yet
+        found = 0; 
+        //fetch the datablock entries
+        //look through current inodes data blocks
+        for(i=0; i < N_BLOCKS; i++){
+            //gets the starting offset of the datablocks, and adds the current block offset to get the datablock
+            datablock = (struct wfs_dentry*)(file_system+super_block->d_blocks_ptr+curr_inode->blocks[i]);
+            //compare the current path token to see if it exists in a datablock entry
+            if (strcmp(datablock->name,token) == 0){
+                found = 1;
+                break;
+            }
+        }
+        //if the path was never found, it does not exist
+        if(!found){
+            return -1;
+        }
 
+        //if it is found, update the current node, TODO, make sure this arithmetic is correct.
+        curr_inode = (struct wfs_inode *) (file_system + super_block->i_blocks_ptr + (datablock->num*sizeof(struct wfs_inode)));
+        
+    }
+
+    // free(copy_path);
+    return 0;
+}
 
 // this fuse operation makes a directory
 static int my_getattr(const char *path, struct stat *stbuf) {
+    struct wfs_inode inode;
+    get_inode("path",&inode);
+
     printf("the stat function is now running\n");
     memset(stbuf, 0, sizeof(struct stat));
     if (strcmp(path, "/") == 0) { // Assuming '/' is your root directory
@@ -71,13 +112,11 @@ int main(int argc, char ** argv) {
     fstat(fd,&stat);
     off_t size = stat.st_size;
 
-    file_system = mmap(NULL, size, PROT_WRITE | PROT_READ, MAP_SHARED, fd, 0); // Corrected mmap call    int test = file_system->d_bitmap_ptr;
-    // Accessing the data structure
-    int test = file_system->d_bitmap_ptr;
-    printf("bitmap pointer is: %d\n", test);
-    
-    close(fd);
+    //setup pointers
+    file_system = mmap(NULL, size, PROT_WRITE | PROT_READ, MAP_SHARED, fd, 0); // Corrected mmap call
+    super_block = (struct wfs_sb*) file_system;
 
+    close(fd);
     // remove disk image path from args to give to fuse main
     char ** fuse_args = (char **) malloc((argc-1) * sizeof(char *));
     fuse_args[0] = argv[0];
